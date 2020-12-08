1faf849ebffc80f46358586deee845144e963898
Rahal2020 <rahal.hasnia@gmail.com>
2020-12-08 10:49:48 +0100

    first commit

---
+++ src/components/profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Address = ()=> { 
+    return (
+        <div> 
+            <h1 className="title"> E-mail: rahal.hasnia@gmail.com</h1>
+</div> 
+    );
+};
+export default Address;
---
+++ src/components/profile/FullName.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FullName = ()=> { 
+    return ( 
+<div>
+<h1 className="title">Rahal Hasnia</h1>
+</div> 
+)
+};
+export default FullName;
